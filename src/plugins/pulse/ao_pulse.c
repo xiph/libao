8cd205adddfefef95481f47e7134944c799d655b
Ralph Giles <giles@xiph.org>
2006-07-18 19:58:56 +0000

    Update for polyp->pulse rename, part two.
    git-svn-id: http://svn.xiph.org/trunk/ao@11706 0101bb08-14d6-0310-b084-bc0e0c8e3800

--- src/plugins/pulse/ao_pulse.c
+++ src/plugins/pulse/ao_pulse.c
@@ -1,20 +1,20 @@
-/* $Id: ao_polyp.c 13 2004-09-19 23:40:00Z lennart $ */
+/* $Id: ao_pulse.c 36 2006-07-14 00:05:13Z lennart $ */
 
 /***
-  This file is part of libao-polyp.
+  This file is part of libao-pulse.
  
-  libao-polyp is free software; you can redistribute it and/or modify
+  libao-pulse is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published
   by the Free Software Foundation; either version 2 of the License,
   or (at your option) any later version.
  
-  libao-polyp is distributed in the hope that it will be useful, but
+  libao-pulse is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
   General Public License for more details.
  
   You should have received a copy of the GNU General Public License
-  along with libao-polyp; if not, write to the Free Software
+  along with libao-pulse; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
   USA.
 ***/
@@ -28,36 +28,34 @@
 #include <signal.h>
 #include <limits.h>
 
-#include <polyp/polyplib-simple.h>
+#include <pulse/simple.h>
+#include <pulse/util.h>
+
 #include <ao/ao.h>
 #include <ao/plugin.h>
 
 /* Unfortunately libao doesn't allow "const" for these structures... */
-static char * ao_polyp_options[] = {
+static char * ao_pulse_options[] = {
     "server",
     "sink"
 };
 
-static ao_info ao_polyp_info = {
+static ao_info ao_pulse_info = {
     AO_TYPE_LIVE,
-    "polypaudio output",
-    "polyp",
+    "PulseAudio Output",
+    "pulse",
     PACKAGE_BUGREPORT,
-    "Outputs to the Polypaudio Sound Server",
+    "Outputs to the PulseAudio Sound Server",
     AO_FMT_NATIVE,
     41,
-    ao_polyp_options,
+    ao_pulse_options,
     2
 };
 
-typedef struct ao_polyp_internal {
+typedef struct ao_pulse_internal {
     struct pa_simple *simple;
     char *server, *sink;
-} ao_polyp_internal;
-
-/* Dirty trick: import these two functions from polyplib */
-char *pa_get_binary_name(char *s, size_t l);
-char *pa_path_get_filename(const char *p);
+} ao_pulse_internal;
 
 /* Yes, this is very ugly, but required nonetheless... */
 static void disable_sigpipe(void) {
@@ -73,7 +71,8 @@ static void disable_sigpipe(void) {
 }
 
 int ao_plugin_test(void) {
-    char p[PATH_MAX], t[256], t2[256], *fn = NULL;
+    char p[PATH_MAX], t[256], t2[256];
+    const char *fn;
     struct pa_simple *s;
     static const struct pa_sample_spec ss = {
         .format = PA_SAMPLE_S16LE,
@@ -83,16 +82,15 @@ int ao_plugin_test(void) {
 
     disable_sigpipe();
     
-    if (getenv("POLYP_SERVER") || getenv("POLYP_SINK"))
+    if (getenv("PULSE_SERVER") || getenv("PULSE_SINK"))
         return 1;
 
-    if (pa_get_binary_name(p, sizeof(p))) {
-        fn = pa_path_get_filename(p);
+    if ((fn = pa_get_binary_name(p, sizeof(p)))) {
         snprintf(t, sizeof(t), "libao[%s]", fn);
         snprintf(t2, sizeof(t2), "libao[%s] test", fn);
     }
 
-    if (!(s = pa_simple_new(NULL, fn ? t : "libao", PA_STREAM_PLAYBACK, NULL, fn ? t2 : "libao test", &ss, NULL, PA_VOLUME_NORM, NULL)))
+    if (!(s = pa_simple_new(NULL, fn ? t : "libao", PA_STREAM_PLAYBACK, NULL, fn ? t2 : "libao test", &ss, NULL, NULL, NULL)))
         return 0;
 
     pa_simple_free(s);
@@ -100,14 +98,14 @@ int ao_plugin_test(void) {
 }
 
 ao_info *ao_plugin_driver_info(void) {
-    return &ao_polyp_info;
+    return &ao_pulse_info;
 }
 
 int ao_plugin_device_init(ao_device *device) {
-    ao_polyp_internal *internal;
+    ao_pulse_internal *internal;
     assert(device);
 
-    internal = (ao_polyp_internal *) malloc(sizeof(ao_polyp_internal));
+    internal = (ao_pulse_internal *) malloc(sizeof(ao_pulse_internal));
     
     if (internal == NULL)	
         return 0; 
@@ -121,9 +119,9 @@ int ao_plugin_device_init(ao_device *device) {
 }
 
 int ao_plugin_set_option(ao_device *device, const char *key, const char *value) {
-    ao_polyp_internal *internal;
+    ao_pulse_internal *internal;
     assert(device && device->internal && key && value);
-    internal = (ao_polyp_internal *) device->internal;
+    internal = (ao_pulse_internal *) device->internal;
     
     if (!strcmp(key, "server")) {
         free(internal->server);
@@ -138,13 +136,14 @@ int ao_plugin_set_option(ao_device *device, const char *key, const char *value) 
 }
 
 int ao_plugin_open(ao_device *device, ao_sample_format *format) {
-    char p[PATH_MAX], t[256], t2[256], *fn = NULL;
-    ao_polyp_internal *internal;
+    char p[PATH_MAX], t[256], t2[256];
+    const char *fn = NULL;
+    ao_pulse_internal *internal;
     struct pa_sample_spec ss;
 
     assert(device && device->internal && format);
 
-    internal = (ao_polyp_internal *) device->internal;
+    internal = (ao_pulse_internal *) device->internal;
 
     if (format->bits == 8)
         ss.format = PA_SAMPLE_U8;
@@ -167,7 +166,7 @@ int ao_plugin_open(ao_device *device, ao_sample_format *format) {
         snprintf(t2, sizeof(t2), "libao[%s] playback stream", fn);
     }
     
-    if (!(internal->simple = pa_simple_new(internal->server, fn ? t : "libao", PA_STREAM_PLAYBACK, internal->sink, fn ? t2 : "libao playback stream", &ss, NULL, PA_VOLUME_NORM, NULL)))
+    if (!(internal->simple = pa_simple_new(internal->server, fn ? t : "libao", PA_STREAM_PLAYBACK, internal->sink, fn ? t2 : "libao playback stream", &ss, NULL, NULL, NULL)))
         return 0;
 
     device->driver_byte_format = AO_FMT_NATIVE;
@@ -176,7 +175,7 @@ int ao_plugin_open(ao_device *device, ao_sample_format *format) {
 
 int ao_plugin_play(ao_device *device, const char* output_samples, uint_32 num_bytes) {
     assert(device && device->internal);
-    ao_polyp_internal *internal = (ao_polyp_internal *) device->internal;
+    ao_pulse_internal *internal = (ao_pulse_internal *) device->internal;
 
     return pa_simple_write(internal->simple, output_samples, num_bytes, NULL) >= 0;
 }
@@ -184,7 +183,7 @@ int ao_plugin_play(ao_device *device, const char* output_samples, uint_32 num_by
 
 int ao_plugin_close(ao_device *device) {
     assert(device && device->internal);
-    ao_polyp_internal *internal = (ao_polyp_internal *) device->internal;
+    ao_pulse_internal *internal = (ao_pulse_internal *) device->internal;
 
     pa_simple_drain(internal->simple, NULL);
     pa_simple_free(internal->simple);
@@ -195,7 +194,7 @@ int ao_plugin_close(ao_device *device) {
 
 void ao_plugin_device_clear(ao_device *device) {
     assert(device && device->internal);
-    ao_polyp_internal *internal = (ao_polyp_internal *) device->internal;
+    ao_pulse_internal *internal = (ao_pulse_internal *) device->internal;
 
     free(internal->server);
     free(internal->sink);
